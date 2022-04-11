4ae0f89192cf3b5d78f530983fa96778fd05ca11
zannatreya <zannatulbintabahar@yahoo.com>
2022-04-11 12:19:02 +0600

    shipping form updated

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import './login.css';
 
@@ -8,6 +8,9 @@ const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate('');
+    const location = useLocation();
+    const from = location.state?.from?.pathname || '/';
+
 
     const [
         signInWithEmailAndPassword,
@@ -23,7 +26,7 @@ const Login = () => {
         setPassword(event.target.value);
     }
     if (user) {
-        navigate('/shop');
+        navigate(from, { replace: true });
     }
 
     const handleUserSignIn = (event) => {
